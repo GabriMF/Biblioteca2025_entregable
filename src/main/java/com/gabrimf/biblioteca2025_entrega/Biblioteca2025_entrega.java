4e7a42dff28cb654d0be76f961040391a96e835b
GabriMF <gabrimf25@gmail.com>
2025-01-15 11:36:51 +0100

    First commit

---
+++ src/main/java/com/gabrimf/biblioteca2025_entrega/Biblioteca2025_entrega.java
@@ -0,0 +1,21 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ */
+
+package com.gabrimf.biblioteca2025_entrega;
+
+import java.time.LocalDate;
+import java.time.temporal.ChronoUnit;
+import java.util.ArrayList;
+import java.util.Scanner;
+
+/**
+ *
+ * @author gabri
+ */
+public class Biblioteca2025_entrega {
+
+    public static void main(String[] args) {
+        System.out.println("Hello World!");
+    }
+}
